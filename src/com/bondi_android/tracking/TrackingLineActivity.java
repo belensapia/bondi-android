df1d7f187a5638059745f36d5b46cba442d26b68
marcosmercuri <marcosmercuri@gmail.com>
2013-07-20 14:46:56 -0300

    Location tracking finished
    The location of the user is now correctly updated regulary.

--- src/com/bondi_android/tracking/TrackingLineActivity.java
+++ src/com/bondi_android/tracking/TrackingLineActivity.java
@@ -1,13 +1,13 @@
 package com.bondi_android.tracking;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -15,32 +15,17 @@ import android.widget.Toast;
 
 import com.bondi_android.R;
 import com.bondi_android.checkin.CheckinActivity;
-import com.bondi_android.util.BondiConstants;
-import com.bondi_android.util.ErrorDialogFragment;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
-import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.location.LocationClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 
-public class TrackingLineActivity extends FragmentActivity implements
-		ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+public class TrackingLineActivity extends Activity implements LocationListener {
+	private static final int TWO_MINUTES = 1000 * 60 * 2;
+	private static final int UPDATE_RATE = 100;
 
-	/*
-	 * Define a request code to send to Google Play services This code is
-	 * returned in Activity.onActivityResult
-	 */
-	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-
+	private LocationManager locationManager;
+	private Location currentLocation;
 	private TextView text;
-	private LocationClient mLocationClient;
-	// Define an object that holds accuracy and frequency parameters
-	private LocationRequest mLocationRequest;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tracking_line);
 
@@ -51,23 +36,73 @@ public class TrackingLineActivity extends FragmentActivity implements
 				.getStringExtra(CheckinActivity.SELECTED_LINE);
 		text.setText(text.getText() + selectedLine);
 
-		// Set updates rate parameters
-		mLocationRequest = LocationRequest.create();
-		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-		mLocationRequest.setInterval(BondiConstants.UPDATE_INTERVAL);
-		mLocationRequest.setFastestInterval(BondiConstants.FASTEST_INTERVAL);
+		// Getting LocationManager object
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-		// Start the location service.
-		mLocationClient = new LocationClient(this, this, this);
-		mLocationClient.connect();
+		registerNewBestProvider(null);
+	}
 
+	private void registerNewBestProvider(String oldProvider) {
+		Criteria criteria = new Criteria();
+		// Getting the name of the provider that meets the criteria
+		String newProvider = locationManager.getBestProvider(criteria, false);
+
+		if (newProvider != null && !newProvider.equals("") && !newProvider.equals(oldProvider)) {
+			if (oldProvider!=null && locationManager!=null) {
+				locationManager.removeUpdates(this);
+			}
+
+			// Get the location from the given provider
+			currentLocation = locationManager.getLastKnownLocation(newProvider);
+
+			locationManager.requestLocationUpdates(newProvider, UPDATE_RATE, 1, this);
+
+			if (currentLocation != null)
+				onLocationChanged(currentLocation);
+			else
+				Toast.makeText(getBaseContext(), "No location was cached",
+						Toast.LENGTH_SHORT).show();
+			return;
+		} 
+
+		//If here, there's no new provider.
+		if (oldProvider==null) {
+			Toast.makeText(getBaseContext(), "No Provider Found",
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	@Override
+	protected void onDestroy() {
+		if (locationManager != null) {
+			locationManager.removeUpdates(this);
+		}
+		Toast.makeText(this, "Disconnected", Toast.LENGTH_SHORT).show();
+		super.onDestroy();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tracking_line, menu);
 		return true;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		if (this.isBetterLocation(location, currentLocation)) {
+			// Report to the UI that the location was updated
+			String msg = "Updated Location: "
+					+ Double.toString(location.getLatitude()) + ","
+					+ Double.toString(location.getLongitude());
+			Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+			/*
+			 * TODO OBS: If your app accesses the network or does other
+			 * long-running work after receiving a location update, adjust the
+			 * fastest interval to a slower value. This prevents your app from
+			 * receiving updates it can't use. Once the long-running work is
+			 * done, set the fastest interval back to a fast value.
+			 */
+		}
 	}
 
 	public void stopTracking(View view) {
@@ -76,147 +111,83 @@ public class TrackingLineActivity extends FragmentActivity implements
 	}
 
 	/**
-	 * When it couldn't connect to the service location.
+	 * Determines whether one Location reading is better than the current
+	 * Location fix
+	 * 
+	 * @param location
+	 *            The new Location that you want to evaluate
+	 * @param currentBestLocation
+	 *            The current Location fix, to which you want to compare the new
+	 *            one
 	 */
-	@Override
-	public void onConnectionFailed(ConnectionResult connectionResult) {
-		/*
-		 * Google Play services can resolve some errors it detects. If the error
-		 * has a resolution, try sending an Intent to start a Google Play
-		 * services activity that can resolve error.
-		 */
-		if (connectionResult.hasResolution()) {
-			try {
-				// Start an Activity that tries to resolve the error
-				connectionResult.startResolutionForResult(this,
-						CONNECTION_FAILURE_RESOLUTION_REQUEST);
-				/*
-				 * Thrown if Google Play services canceled the original
-				 * PendingIntent
-				 */
-			} catch (IntentSender.SendIntentException e) {
-				// Log the error
-				e.printStackTrace();
-			}
-		} else {
-			/*
-			 * If no resolution is available, display a dialog to the user with
-			 * the error.
-			 */
-			showErrorDialog(connectionResult.getErrorCode());
-		}
-	}
-
-	@Override
-	protected void onDestroy() {
-		if (mLocationClient.isConnected()) {
-            mLocationClient.removeLocationUpdates(this);
-        }
-		mLocationClient.disconnect();
-		Toast.makeText(this, "Disconnected", Toast.LENGTH_SHORT).show();
-		super.onDestroy();
-	}
-
-	/**
-	 * Called by Location Services when the request to connect the client
-	 * finishes successfully.
-	 */
-	@Override
-	public void onConnected(Bundle bundle) {
-		// Display the connection status
-		// TODO is this necessary? No, the toast its just a pop up msg.
-		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-
-		// start tracking
-		if (isServicesConnected()) {
-//			currentLocation = mLocationClient.getLastLocation();
-			mLocationClient.requestLocationUpdates(mLocationRequest, this);
-		}
-
-	}
-
-	/**
-	 * Called by Location Services if the connection to the location client
-	 * drops because of an error.
-	 */
-	@Override
-	public void onDisconnected() {
-		Toast.makeText(this, "Disconnected. Please re-connect.",
-				Toast.LENGTH_SHORT).show();
-	}
-
-	/**
-	 * Handle results returned to the FragmentActivity by Google Play services
-	 */
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// Decide what to do based on the original request code
-		switch (requestCode) {
-
-		case CONNECTION_FAILURE_RESOLUTION_REQUEST:
-			/*
-			 * If the result code is Activity.RESULT_OK, try to connect again
-			 */
-			switch (resultCode) {
-			case Activity.RESULT_OK:
-				// Try the request again
-				mLocationClient.connect();
-				break;
-			}
-
-		}
-	}
-
-	private boolean isServicesConnected() {
-		// Check that Google Play services is available
-		int resultCode = GooglePlayServicesUtil
-				.isGooglePlayServicesAvailable(this);
-		// If Google Play services is available
-		if (ConnectionResult.SUCCESS == resultCode) {
-			// In debug mode, log the status
-			Log.d("Location Updates", "Google Play services is available.");
-			// Continue
+	protected boolean isBetterLocation(Location location,
+			Location currentBestLocation) {
+		if (currentBestLocation == null) {
+			// A new location is always better than no location
 			return true;
-			// Google Play services was not available for some reason
-		} else {
-			// Get the error code
-			showErrorDialog(resultCode);
+		}
+
+		// Check whether the new location fix is newer or older
+		long timeDelta = location.getTime() - currentBestLocation.getTime();
+		boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+		boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+		boolean isNewer = timeDelta > 0;
+
+		// If it's been more than two minutes since the current location, use
+		// the new location
+		// because the user has likely moved
+		if (isSignificantlyNewer) {
+			return true;
+			// If the new location is more than two minutes older, it must be
+			// worse
+		} else if (isSignificantlyOlder) {
 			return false;
 		}
-	}
 
-	private void showErrorDialog(int errorCode) {
-		// Get the error dialog from Google Play services
-		Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(errorCode,
-				this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+		// Check whether the new location fix is more or less accurate
+		int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation
+				.getAccuracy());
+		boolean isLessAccurate = accuracyDelta > 0;
+		boolean isMoreAccurate = accuracyDelta < 0;
+		boolean isSignificantlyLessAccurate = accuracyDelta > 200;
 
-		// If Google Play services can provide an error dialog
-		if (errorDialog != null) {
-			// Create a new DialogFragment for the error dialog
-			ErrorDialogFragment errorFragment = new ErrorDialogFragment();
-			// Set the dialog in the DialogFragment
-			errorFragment.setDialog(errorDialog);
-			// Show the error dialog in the DialogFragment
-			errorFragment.show(getSupportFragmentManager(), "Location Updates");
+		// Check if the old and new location are from the same provider
+		boolean isFromSameProvider = isSameProvider(location.getProvider(),
+				currentBestLocation.getProvider());
+
+		// Determine location quality using a combination of timeliness and
+		// accuracy
+		if (isMoreAccurate) {
+			return true;
+		} else if (isNewer && !isLessAccurate) {
+			return true;
+		} else if (isNewer && !isSignificantlyLessAccurate
+				&& isFromSameProvider) {
+			return true;
 		}
+		return false;
 	}
 
-	// Define the callback method that receives location updates
+	/** Checks whether two providers are the same */
+	private boolean isSameProvider(String provider1, String provider2) {
+		if (provider1 == null) {
+			return provider2 == null;
+		}
+		return provider1.equals(provider2);
+	}
+
 	@Override
-	public void onLocationChanged(Location location) {
-		// Report to the UI that the location was updated
-		//TODO Is not getting updates. Why?!
-		String msg = "Updated Location: "
-				+ Double.toString(location.getLatitude()) + ","
-				+ Double.toString(location.getLongitude());
-		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-		
-		/* TODO
-		 * OBS: If your app accesses the network or does other long-running work
-		 * after receiving a location update, adjust the fastest interval to a
-		 * slower value. This prevents your app from receiving updates it can't
-		 * use. Once the long-running work is done, set the fastest interval
-		 * back to a fast value.
-		 */
+	public void onProviderDisabled(String provider) {
+		registerNewBestProvider(provider);
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
 	}
 }
